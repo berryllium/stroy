dc915252020a0e384ebaa79b98e80e6bc64e0f3c
berryllium <gorkundp@yandex.ru>
2019-10-19 14:57:48 +0300

    скрипт калькулятора добавлен

---
+++ js/calc.js
@@ -0,0 +1,16 @@
+function headerCalc() {
+  $('.inp_range_2').on("change mousemove", function () {
+    $('.head_form__count2').val($(this).val());
+  });
+  $('.inp_range_1').on("change mousemove", function () {
+    $('.head_form__count1').val($(this).val());
+  });
+
+  $('.head_form__count2').on("change", function () {
+    $('.inp_range_2').val($(this).val());
+  });
+  $('.head_form__count1').on("change", function () {
+    $('.inp_range_1').val($(this).val());
+  });
+};
+headerCalc();
